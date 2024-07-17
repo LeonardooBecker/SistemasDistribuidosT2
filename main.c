549e638f87b61adfbf90dfd645ef85b37984da1c
lbo21 <leonardobecker79@gmai.com>
2024-07-16 21:28:43 -0300

    init detect fault

--- main.c
+++ main.c
@@ -25,6 +25,30 @@ void imprime_nod(no* nod)
     printf("%d, %d, %d\n", nod->id, nod->s, nod->visitado);
 }
 
+int verifica_processo_falho(node_set* nodes)
+{
+    int qnt_processos_falhos = 4;
+    int *processosFalhos = (int*)malloc(sizeof(int)*qnt_processos_falhos);
+    processosFalhos = (int[]){20,20,20,20};
+    for(int j = 0; j < nodes->size; j++)
+    {
+        int existeFalho = 0;
+        for(int i = 0; i < qnt_processos_falhos; i++)
+        {
+            if(nodes->nodes[j] == processosFalhos[i])
+            {
+                existeFalho = 1;
+                break;
+            }
+        }
+        if(!existeFalho)
+        {
+            return nodes->nodes[j];
+        }
+    }
+    return -1;
+}
+
 void narvore(int raiz, int s, int dimensao)
 {
     node_set* nodes;
@@ -33,13 +57,13 @@ void narvore(int raiz, int s, int dimensao)
     nod->s = s;
     if(nod->id == RAIZ)
     {
-        nodes = cis(raiz,s);
-        narvore(nodes->nodes[0],s,dimensao);
-        while(nod->s < dimensao)
+        imprime_nod(nod);
+        while(nod->s <= dimensao)
         {
-            nodes = cis(raiz,nod->s+1);
+            nodes = cis(raiz,nod->s);
+            int no_oficial = verifica_processo_falho(nodes);
+            narvore(no_oficial,nod->s,dimensao);
             nod->s += 1;
-            narvore(nodes->nodes[0],nod->s,dimensao);
         }
     }
     else
@@ -48,18 +72,20 @@ void narvore(int raiz, int s, int dimensao)
         {
             for(int i = 2; i <= nod->s; i++)
             {
-                if(!nod->visitado)
+                if(!nod->visitado && nod->id != -1)
                 {
                     imprime_nod(nod);
                 }
                 nod->visitado = 1;
                 nodes = cis(nod->id,i-1);
-                narvore(nodes->nodes[0],i-1,dimensao);
+                int no_oficial = verifica_processo_falho(nodes);
+                narvore(no_oficial,i-1,dimensao);
             }
         }
         else
         {
-            imprime_nod(nod);
+            if(nod->id != -1)
+                imprime_nod(nod);
         }
     }
 
@@ -68,7 +94,7 @@ void narvore(int raiz, int s, int dimensao)
 
 int main()
 {
-    int raiz = 0;
+    int raiz = RAIZ;
     int dimensao = 4;
     narvore(raiz,1,dimensao);
 
